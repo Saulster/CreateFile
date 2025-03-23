968794ea4eb5c5071dcc6ea0ccfc350f183c1207
Salih Adams <salihadams2003@gmail.com>
2025-03-23 20:29:39 +0200

    Add files via upload

---
+++ CreateFile/src/createfile/CreateFile.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package createfile;
+
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+public class CreateFile
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+
+
+ {
+    try {
+      File myObj = new File("filename.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
